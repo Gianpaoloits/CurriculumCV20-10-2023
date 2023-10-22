e9360b7ace131f3fc048098f0d210658ee20e773
studente <gianpaolo.gabancho.its@gmail.com>
2023-10-22 22:02:10 +0200

    ho finito l'esercizio

---
+++ curriculum/src/model/Profilo.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { personaType, livelloLingua, esperienzaPersona, personaHobby } from './ProfiloProps';
+import { person, lingueTotali, esperiezeTotali, hobbyTotali } from './ProfiloData';
+
+function Profilo() {
+    return (
+        <div className="card">
+            <h1>Profilo</h1>
+            <p>Nome: {person.nome}</p>
+            <p>Cognome: {person.cognome}</p>
+            <img src={person.imgProfilo} alt={`${person.nome} ${person.cognome}`} />
+            <p>Età: {person.eta}</p>
+            <p>Sesso: {person.sesso}</p>
+            <p>Residenza: {person.residenza}</p>
+            {person.numeroDiTelefono && <p>Numero di Telefono: {person.numeroDiTelefono}</p>}
+            <h2>Lingue</h2>
+            <ul>
+                {lingueTotali.map((lingua: livelloLingua, index: number) => (
+                    <li key={index}>{lingua.lingua} - Livello: {lingua.livelloLingua}</li>
+                ))}
+            </ul>
+            <h2>Esperienze Lavorative</h2>
+            <ul>
+                {esperiezeTotali.map((esperienza: esperienzaPersona, index: number) => (
+                    <li key={index}>
+                        {esperienza.tipoDiImpiego} presso {esperienza.datoreDiLavoro} ({esperienza.annoInizio} - {esperienza.annoFine})
+                    </li>
+                ))}
+            </ul>
+            <h2>Hobby</h2>
+            <ul>
+                {hobbyTotali.map((hobby: personaHobby, index: number) => (
+                    <li key={index}>{hobby.hobby}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Profilo;
