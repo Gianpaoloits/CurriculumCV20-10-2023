c8895c00c2bfea3a8f585fbbca3852a981eb5b24
studente <gianpaolo.gabancho.its@gmail.com>
2023-10-23 09:16:16 +0200

    Ho fatto delle modifiche ad App.tsx

--- curriculum/src/App.tsx
+++ curriculum/src/App.tsx
@@ -4,7 +4,7 @@ import Profilo from './model/Profilo';
 function App() {
   return (
     <div className="card">
-        <Profilo /> {/* Includiamo il componente Profilo */}
+        <Profilo />
     </div>
   );
 }
